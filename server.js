04b43395820c7a9c05bd16978c856bc1c49fe3df
Dominique Clark <melaydominique@gmail.com>
2024-01-12 13:19:07 -0800

    get route - hello world - displays on localhost3000

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+
 app.listen(PORT, () => {
     console.log('app is running on port: ', PORT)
+})
+
+//greetings 
+app.get('/', (req, res) => {
+    res.send('Hello Wolrd!')
 })
