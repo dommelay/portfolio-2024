e848e62f71b409f4d7bcd3ca299c56b85b8c5814
Dominique Clark <melaydominique@gmail.com>
2024-01-14 14:33:39 -0800

    contact form - npm i emailjs-com

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useState} from 'react'
+import emailjs from 'emailjs-com'
 
 
 const Contact = () => 
--- frontend/src/components/Work.js
+++ frontend/src/components/Work.js
@@ -7,6 +7,8 @@ const Work = () =>
     return (
         <div id='testcontainer'>
             <h1>WORK PAGE</h1>
+            <img className='workimg' src='/images/weather.jpg' alt='Weather App'/>
+            <img className='workimg' src='/images/gallery.jpg' alt='Gallery'/>
         </div>
 
     )
