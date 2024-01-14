619d4d18652c214bd769090bc529afca46697923
Dominique Clark <melaydominique@gmail.com>
2024-01-13 16:38:12 -0800

    sticky nav bar styled and responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useState, useEffect} from 'react'
-import Navagation from './components/Navigation'
+import {Link} from 'react-router-dom'
+import Navigation from './components/Navigation'
 import './App.css'
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
   return (
 
     <div id='homecontainer'>
+      <Navigation/>
       <h1>Hello World!</h1>
     </div>
 
--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useState} from 'react'
-
+import {Link, BrowserRouter, Routes, Route} from 'react-router-dom'
 
 const Navigation = () => {
 
@@ -9,10 +9,25 @@ const Navigation = () => {
 
     return (
     <div id='navcontainer'>
-        
 
+        <div id='navbox'>
+            <h2 className='navitem'>home</h2>
+            <h2 className='navitem'>about</h2>
+            <h2 className='navitem'>work</h2>
+            <h2 className='navitem'>contact</h2>
+        </div>
+
+        <div id='navnamebox'>
+            <h3 id='navname'>Dominique Melay</h3>
+        </div>
 
     </div>
 
     )
 }
+
+export default Navigation;
+
+   {/* <Link to ={'/'}>
+            <h2 className='navitem'>Home</h2>
+        </Link> */}
