6eac7a4f892842f02565ddafdf79d267f1ac636c
Dominique Clark <melaydominique@gmail.com>
2024-01-15 14:07:03 -0800

    opaque hover over images of work intact to insinutae hyperlink to frontend repo upon clock; use of ternaries and mouse events

--- frontend/src/components/Work.js
+++ frontend/src/components/Work.js
@@ -2,19 +2,41 @@ import React from 'react'
 import {useState} from 'react'
 
 
-const Work = () => 
-{
+const Work = () => {
+const [weatherHover, setWeatherHover] = useState(false)
+
+const handleWeatherHover = () => {
+    setWeatherHover(!weatherHover)
+}
+
+
     return (
         <div id='testcontainer'>
             <div className='pagetitle'>
                 <h1>WORK PAGE</h1>
             </div>
             <div id='workcontainer'>
-                <a href='https://github.com/dommelay/WeatherApp-frontend' target='_blank'>
-                    <img className='workimg' src='/images/weather.jpg' alt='Weather App'/>
-                </a>
-                <img className='workimg' src='/images/gallery.jpg' alt='Gallery'/>
-                <img className='workimg' src='/images/gallery.jpg' alt='Gallery'/>
+                <div onMouseOver={handleWeatherHover} className='workboxtrue'>
+                    <a href='https://github.com/dommelay/WeatherApp-frontend' target='_blank'>
+                        <img className='workimg' src='/images/weather.jpg' alt='Weather App'/>
+                    </a>
+                </div>
+            { weatherHover ? 
+            <>
+            <div className='opaque'></div>
+           
+            </>
+                :
+                <></>
+                // <div className='workboxfalse'>
+                //     <a href='https://github.com/dommelay/WeatherApp-frontend' target='_blank'>
+                //         <img className='workimg' src='/images/weather.jpg' alt='Weather App'/>
+                //     </a>
+                // </div>
+            }
+
+                {/* <img className='workimg' src='/images/gallery.jpg' alt='Gallery'/>
+                <img className='workimg' src='/images/gallery.jpg' alt='Gallery'/> */}
             </div>
         </div>
 
