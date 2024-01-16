9421dec24d77398a22f9e594d49300c2e14d4b0b
Dominique Clark <melaydominique@gmail.com>
2024-01-16 15:39:58 -0800

    emailjs contact form is LIVE and working!!!!!

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {useState} from 'react'
-import emailjs from 'emailjs-com'
+import {useState, useEffect} from 'react'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
 const [formData, setFormData] = useState({
@@ -8,22 +8,33 @@ const [formData, setFormData] = useState({
     from_email: '',
     message: ''
 })
-
+// useEffect(() => {
+//     console.log('testing')
+// })
 const handleChange = (e) => {
     e.preventDefault()
     setFormData({...formData, [e.target.name]: e.target.value})
+}
+const handleSubmit = (e) => {
+    e.preventDefault()
+    emailjs.send('service_9drkbdz', 'contact_form', formData, 'uebwbWRR7EBbkwtG5').then((response) => {
+        console.log(formData)
+    }).catch((error) => {
+        console.log('error')
+    })
 }
     return (
         <div id='testcontainer'>
            
             <div id='contactcontainer'>
-                <form id='form'>
+                <form id='form' onSubmit={handleSubmit}>
                     <label htmlFor='from_name'>Name:</label>
                         <input type='text' name='from_name' value={formData.from_name} onChange={handleChange}></input>
                     <label htmlFor='from_email'>Email:</label>
                         <input type='text'name='from_email' value={formData.from_email} onChange={handleChange}></input>
                     <label htmlFor='message'>Message:</label>
                         <input type='text' name='message' value={formData.message} onChange={handleChange}></input>
+                    <button type='submit'>submit</button>
                 </form>
             </div>
 
