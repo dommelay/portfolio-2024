6664fe6f3e74aa5f35afcf51fa11992a9cfb70a0
Dominique Clark <melaydominique@gmail.com>
2024-01-15 16:10:45 -0800

    gallery opaque hyperlink added

--- frontend/src/components/Work.js
+++ frontend/src/components/Work.js
@@ -4,12 +4,19 @@ import {useState} from 'react'
 
 const Work = () => {
 const [weatherHover, setWeatherHover] = useState(false)
+const [galleryHover, setGalleryHover] = useState(false)
 
 const handleWeatherHover = () => {
     setWeatherHover(true)
 }
 const handleWeatherHoverOff = () => {
     setWeatherHover(false)
+}
+const handleGalleryHover = () => {
+    setGalleryHover(true)
+}
+const handleGalleryHoverOff = () => {
+    setGalleryHover(false)
 }
 
 
@@ -19,7 +26,7 @@ const handleWeatherHoverOff = () => {
             <div id='workcontainer'>
 
                 <div id='weatherbox'>
-                    <div onMouseEnter={handleWeatherHover} className='workboxtrue'>
+                    <div onMouseEnter={handleWeatherHover} className='weatherboxtrue'>
                         <img className='workimg' src='/images/weather.jpg' alt='Weather App'/>
                     </div>
             { weatherHover ? 
@@ -28,9 +35,20 @@ const handleWeatherHoverOff = () => {
                     <div onMouseLeave={handleWeatherHoverOff} className='opaque'></div>
                 </a>
             </>
-                :
-                <></>
-            }
+                : <></> }
+                </div>
+
+                <div id='artbox'>
+                    <div onMouseEnter={handleGalleryHover} className='artboxtrue'>
+                        <img className='workimg' src='/images/gallery.jpg' alt='Gallery'></img>
+                    </div>
+                { galleryHover ?
+                <>
+                    <a href='https://github.com/dommelay/GalleryFront2' target='_blank'>
+                        <div onMouseLeave={handleGalleryHoverOff} className='opaque'></div>
+                    </a>
+                </>
+                : <></>}
                 </div>
 
             </div>
