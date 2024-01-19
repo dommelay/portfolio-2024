7dc4a3a1f72cfb46338eeb3faf691a4958f70b3f
Dominique Clark <melaydominique@gmail.com>
2024-01-18 16:38:54 -0800

    laptop is dying, commit, working on contact form

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -28,13 +28,19 @@ const handleSubmit = (e) => {
            
             <div id='contactcontainer'>
                 <form id='form' onSubmit={handleSubmit}>
-                    <label htmlFor='from_name'>Name:</label>
-                        <input type='text' name='from_name' value={formData.from_name} onChange={handleChange}></input>
-                    <label htmlFor='from_email'>Email:</label>
-                        <input type='text'name='from_email' value={formData.from_email} onChange={handleChange}></input>
-                    <label htmlFor='message'>Message:</label>
-                        <input type='text' name='message' value={formData.message} onChange={handleChange}></input>
-                    <button type='submit'>submit</button>
+                    <div id='fromnamediv' className='labelinput'>
+                        <label className='formlabel' htmlFor='from_name'>Name:</label>
+                        <input id='inputname' type='text' name='from_name' value={formData.from_name} onChange={handleChange}></input>
+                    </div>
+                    <div id='fromemaildiv' className='labelinput'>
+                        <label className='formlabel' htmlFor='from_email'>Email:</label>
+                        <input id='inputemail' type='text'name='from_email' value={formData.from_email} onChange={handleChange}></input>
+                    </div>
+                    <div id='messagediv' className='labelinput'>
+                        <label className='formlabel' htmlFor='message'>Message:</label>
+                        <input id='inputmessage' type='text' name='message' value={formData.message} onChange={handleChange}></input>
+                    </div>
+                    <button className='cursive' type='submit'>submit</button>
                 </form>
             </div>
 
