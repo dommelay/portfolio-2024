75e0567c334c71151b5095c54a73936a8fea3dcf
Dominique Clark <melaydominique@gmail.com>
2024-01-19 16:52:54 -0800

    changing locations

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -7,10 +7,7 @@ const About = () =>
     return (
         <div id='testcontainer'>
             <div className='pagetitle'>
-                <h1>ABOUT PAGE</h1>
-            </div>
-            <div>
-                <img className='workprogimg' src='/images/workinprogress.png' alt='Work in Progress'></img>
+                
             </div>
         </div>
 
