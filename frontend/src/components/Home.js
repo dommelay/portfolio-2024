cf01520e1c024599609670ed91ea50d7ab01f209
Dominique Clark <melaydominique@gmail.com>
2024-01-21 14:38:43 -0800

    restyling home page boredddd

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,24 +5,18 @@ import {useState} from 'react'
 const Home = () => 
 {
     return (
-        <div id='testcontainer'>
-            {/* <div className='pagetitle'>
-                <h1>HOME PAGE</h1>
-            </div> */}
-            <div className='homecontainer'>
-                <div>
-                    <img id='homeshot' src='/images/homeshot.jpg' alt='Head Shot'/>
-                </div>
-                <div id='homeinfobox'>
-                    <h1 className='cursive' id='homeposition'>Software & Product Engineer</h1>
-                    <h2 className='normaltext' id='homename'>Dominique Melay</h2>
-                   
-                    {/* <h3 className='normaltext' id='homebrand'>A Software Engineer and University of Michigan graduate with a solid foundation in Neuroscience, Biomechanics, and the fundamentals of Engineering. Experienced in digital marketing and product design, I bring a diverse skill set that combines technical expertise with a creative and strategic approach; uniquely positioning me to tackle complex problems and deliver innovative solutions in the ever-evolving landscape of software development.</h3> */}
-
-                    
-                    
-                </div>
+        <div id='homecontainerbox'>
+           
+           <div id='homeheader'>
+                <h1 className='cursive' id='hometitle'>Software & Product Engineer</h1>
+           </div>
+            <div id='homeimg'>
+                <img id='homeshot' src='/images/FullSizeRender.jpg' alt='Dominique Melay'></img>
             </div>
+            <div id='homefooter'>
+                <h1 className='cursive'>Dominique Melay</h1>
+            </div>
+
         </div>
 
     )
