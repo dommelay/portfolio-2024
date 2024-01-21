9fda4e5efc9ac406b69e36d38a7fa1f6bf8c6f99
Dominique Clark <melaydominique@gmail.com>
2024-01-21 15:40:42 -0800

    home page styling

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -10,12 +10,29 @@ const Home = () =>
            <div id='homeheader'>
                 <h1 className='cursive' id='hometitle'>Software & Product Engineer</h1>
            </div>
-            <div id='homeimg'>
-                <img id='homeshot' src='/images/FullSizeRender.jpg' alt='Dominique Melay'></img>
+
+            <div id='homemiddle'>
+                {/* <div className='homeinfobox'>
+                    <h1 className='cursive homeinfo flexend'>Freelance</h1>
+                    <h1 className='cursive homeinfo'>Web Development</h1>
+                    <h1 className='cursive homeinfo flexend'>Fashion/Modeling</h1>
+                </div> */}
+                <div id='homeimg'>
+                    <img id='homeshot' src='/images/homeshot.png' alt='Dominique Melay'></img>
+                </div>
+                <div className='homeinfobox'>
+                    <h1 className='normaltext homeinfo center'>Freelance</h1>
+                    <h1 className='normaltext homeinfo'>Web Development</h1>
+                    <h1 className='normaltext homeinfo flexend'>Digital Marketing</h1>
+                    <h1 className='normaltext homeinfo'>Product Design</h1>
+                    <h1 className='normaltext homeinfo flexend'>Fashion/Modeling</h1>
+                    <h1 className='normaltext homeinfo'>Content Creation</h1>
+                </div>
             </div>
-            <div id='homefooter'>
-                <h1 className='cursive'>Dominique Melay</h1>
-            </div>
+
+            {/* <div id='homefooter'>
+                <h1 className='normaltext'>Dominique Melay</h1>
+            </div> */}
 
         </div>
 
