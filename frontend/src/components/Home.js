05801048535d578d02bd319122ee2a5c7cabafc8
Dominique Clark <melaydominique@gmail.com>
2024-01-21 17:17:20 -0800

    home page styled, needs media queries

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useState} from 'react'
+import {Link} from 'react-router-dom'
 
 
 const Home = () => 
@@ -21,13 +22,18 @@ const Home = () =>
                 <div id='homeimg'>
                     <img id='homeshot' src='/images/homeshot.png' alt='Dominique Melay'></img>
                 </div>
-                <div className='homeinfobox'>
-                    <h1 className='normaltext homeinfo center'>Freelance</h1>
-                    <h1 className='normaltext homeinfo'>Web Development</h1>
-                    <h1 className='normaltext homeinfo flexend'>Digital Marketing</h1>
-                    <h1 className='normaltext homeinfo'>Product Design</h1>
-                    <h1 className='normaltext homeinfo flexend'>Fashion/Modeling</h1>
-                    <h1 className='normaltext homeinfo'>Content Creation</h1>
+                <div id='workcalldivhome'>
+                    <div className='homeinfobox'>
+                        <h1 className='normaltext homeinfo center'>Freelance</h1>
+                        <h1 className='normaltext homeinfo'>Web Development</h1>
+                        <h1 className='normaltext homeinfo flexend'>Digital Marketing</h1>
+                        <h1 className='normaltext homeinfo'>Product Design</h1>
+                        <h1 className='normaltext homeinfo flexend'>Fashion/Modeling</h1>
+                        <h1 className='normaltext homeinfo'>Content Creation</h1>
+                    </div>
+                    <Link to={'/contact'}>
+                        <button className='cursive workbttnhome' id='workbttn'>work with me</button>
+                    </Link>
                 </div>
             </div>
 
