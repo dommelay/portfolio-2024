58ae816e066cfb624892cf76a9cc046127830b2e
Dominique Clark <melaydominique@gmail.com>
2024-02-17 23:48:46 -0800

    small changes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () =>
         <div id='homecontainerbox'>
            
            <div id='homeheader'>
-                <h1 className='cursive' id='hometitle'>Software & Product Engineer</h1>
+                <h1 className='cursive' id='hometitle'>Software Engineer</h1>
                 <h1 className='cursive' id='homename'>Dominique Melay</h1>
            </div>
 
